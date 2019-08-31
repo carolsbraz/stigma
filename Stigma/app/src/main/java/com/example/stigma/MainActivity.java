a6ccf5afeaee330a7812d51924d293d7c6739141
carolsbraz <48094755+carolsbraz@users.noreply.github.com>
2019-08-31 14:15:46 -0400

    projeto

---
+++ Stigma/app/src/main/java/com/example/stigma/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.stigma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
