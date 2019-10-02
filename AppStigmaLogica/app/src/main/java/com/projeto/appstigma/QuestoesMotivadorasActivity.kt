9e5aa61a0190ea52a125e2a85464a5be5ffd8afc
Clara Lima <47910144+Claralimab@users.noreply.github.com>
2019-10-02 16:50:15 -0400

    as

--- AppStigmaLogica/app/src/main/java/com/projeto/appstigma/QuestoesMotivadorasActivity.kt
+++ AppStigmaLogica/app/src/main/java/com/projeto/appstigma/QuestoesMotivadorasActivity.kt
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.stigma.R
 
-class PerguntasMotivadorasActivity : AppCompatActivity() {
+class QuestoesMotivadorasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perguntasmotivadoras)
+        setContentView(R.layout.activity_questoesmotivadoras)
     }
+
 }
