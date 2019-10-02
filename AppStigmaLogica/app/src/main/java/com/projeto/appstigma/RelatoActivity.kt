fa15ff08997ddc89a0ecda17a335d583c65045d6
carolsbraz <48094755+carolsbraz@users.noreply.github.com>
2019-10-02 15:52:28 -0400

    .

--- AppStigmaLogica/app/src/main/java/com/projeto/appstigma/RelatoActivity.kt
+++ AppStigmaLogica/app/src/main/java/com/projeto/appstigma/RelatoActivity.kt
@@ -1,11 +1,10 @@
 package com.projeto.appstigma
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.stigma.R
-import com.example.stigma.Relato
-import com.example.stigma.Usuario
+import com.example.stigma.*
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_relato.*
 
@@ -62,8 +61,12 @@ class RelatoActivity : AppCompatActivity() {
                     Toast.LENGTH_SHORT
                 ).show()
             }
-
-
         }
+
+//        btn_ver.setOnClickListener{
+//            val intent = Intent(this, RelatosDiariosActivity::class.java)
+//            startActivity(intent)
+//            this.finish()
+//        }
     }
 }
--- AppStigmaLogica/app/src/main/java/com/projeto/appstigma/RelatosDiariosActivity.kt
+++ AppStigmaLogica/app/src/main/java/com/projeto/appstigma/RelatosDiariosActivity.kt
@@ -2,20 +2,46 @@ package com.projeto.appstigma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.example.stigma.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_principal.*
 import kotlinx.android.synthetic.main.activity_relatos_diarios.*
 
 class RelatosDiariosActivity : AppCompatActivity() {
+
+    var emailLogado = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relatos_diarios)
 
-
         btn_voltar_3.setOnClickListener {
             finish()
         }
+
+        val user = FirebaseAuth.getInstance().currentUser
+        emailLogado = user!!.email.toString()
+
+        for (i in usuariosList){
+            if(i.email == emailLogado){
+                txt_nome_usuario.text = i.nome
+
+            }
+        }
+
+        val relatoAdapter = RelatoAdapter(this)
+
+        list_view_relatos.adapter = relatoAdapter
+
+        for (r in relatosList){
+            if(r.usuario == emailLogado){
+
+                relatoAdapter.addAll(r)
+            }
+        }
+
+
+
 
     }
 }
