cc7e6c01c0da9aa3d85dd1fd3dac1986df26490d
Clara Lima <47910144+Claralimab@users.noreply.github.com>
2019-10-03 13:14:35 -0400

    at5

---
+++ AppStigmaLogica/app/src/main/java/com/projeto/appstigma/DesafioActivity.kt
@@ -0,0 +1,14 @@
+package com.projeto.appstigma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stigma.R
+
+class DesafioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_desafio)
+
+    }
+}
---
+++ AppStigmaLogica/app/src/main/java/com/projeto/appstigma/ListarQuestoesActivity.kt
@@ -0,0 +1,17 @@
+package com.projeto.appstigma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stigma.R
+
+class ListarQuestoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listar_questoes)
+
+    }
+
+
+
+}
