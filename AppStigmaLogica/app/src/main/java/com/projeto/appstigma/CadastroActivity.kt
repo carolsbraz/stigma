a3601e23a8c7a1ae5801437bf9f9611c1f147bc7
carolsbraz <48094755+carolsbraz@users.noreply.github.com>
2019-10-11 13:15:08 -0400

    yo

--- AppStigmaLogica/app/src/main/java/com/projeto/appstigma/CadastroActivity.kt
+++ AppStigmaLogica/app/src/main/java/com/projeto/appstigma/CadastroActivity.kt
@@ -13,12 +13,66 @@ import kotlinx.android.synthetic.main.activity_cadastro.*
 class CadastroActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    var avatar2 = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar!!.hide()
         setContentView(R.layout.activity_cadastro)
         auth = FirebaseAuth.getInstance()
+
+        val extras = intent.extras
+        if(extras != null){
+            avatar2 = extras.getString("avatar").toString()
+            if(avatar2 == "avatar_boy1"){
+                btn_avatar.setBackgroundResource(R.drawable.boy1)
+            }
+            if(avatar2 == "avatar_boy2"){
+                btn_avatar.setBackgroundResource(R.drawable.boy2)
+            }
+            if(avatar2 == "avatar_boy3"){
+                btn_avatar.setBackgroundResource(R.drawable.boy3)
+            }
+            if(avatar2 == "avatar_boy4"){
+                btn_avatar.setBackgroundResource(R.drawable.boy4)
+            }
+            if(avatar2 == "avatar_boy5"){
+                btn_avatar.setBackgroundResource(R.drawable.boy5)
+            }
+            if(avatar2 == "avatar_boy6"){
+                btn_avatar.setBackgroundResource(R.drawable.boy6)
+            }
+            if(avatar2 == "avatar_boy7"){
+                btn_avatar.setBackgroundResource(R.drawable.boy7)
+            }
+            if(avatar2 == "avatar_boy8"){
+                btn_avatar.setBackgroundResource(R.drawable.boy8)
+            }
+            if(avatar2 == "avatar_boy9"){
+                btn_avatar.setBackgroundResource(R.drawable.boy9)
+            }
+
+            if(avatar2 == "avatar_girl1"){
+                btn_avatar.setBackgroundResource(R.drawable.girl1)
+            }
+            if(avatar2 == "avatar_girl2"){
+                btn_avatar.setBackgroundResource(R.drawable.girl2)
+            }
+            if(avatar2 == "avatar_girl3"){
+                btn_avatar.setBackgroundResource(R.drawable.girl3)
+            }
+            if(avatar2 == "avatar_girl4"){
+                btn_avatar.setBackgroundResource(R.drawable.girl4)
+            }
+            if(avatar2 == "avatar_girl5"){
+                btn_avatar.setBackgroundResource(R.drawable.girl5)
+            }
+            if(avatar2 == "avatar_girl6"){
+                btn_avatar.setBackgroundResource(R.drawable.girl6)
+            }
+
+        }
+
         buttonCadastrar.setOnClickListener {
 
             val nome = txt_nome.text.toString()
@@ -26,7 +80,8 @@ class CadastroActivity : AppCompatActivity() {
             val email = txt_email.text.toString()
             val senha = txt_senha.text.toString()
             val confSenha = txt_confSenha.text.toString()
-
+            val extras = intent.extras
+            var avatar = extras?.getString("avatar")
 
             if (senha != confSenha) {
                 txt_confSenha.error = "As senhas não coincidem"
@@ -39,7 +94,7 @@ class CadastroActivity : AppCompatActivity() {
                         .addOnCompleteListener(this) { task ->
                             if (task.isSuccessful) {
                                 val user =
-                                    Usuario(usuarios.push().key!!, nome, email, dataNasc, senha)
+                                    Usuario(usuarios.push().key!!, nome, email, dataNasc, senha, avatar)
                                 usuarios.child(user.id).setValue(user)
                                 auth.signInWithEmailAndPassword(email, senha)
                                     .addOnCompleteListener(this) { task ->
--- AppStigmaLogica/app/src/main/java/com/projeto/appstigma/Usuario.kt
+++ AppStigmaLogica/app/src/main/java/com/projeto/appstigma/Usuario.kt
@@ -6,15 +6,17 @@ class Usuario{
     var email : String = ""
     var dataNasc : String = ""
     var senha : String = ""
+    var avatar : String? = ""
 
     constructor(){
     }
 
-    constructor(id : String, nome : String, email : String, dataNasc : String, senha : String){
+    constructor(id : String, nome : String, email : String, dataNasc : String, senha : String, avatar : String?){
         this.id = id
         this.nome = nome
         this.email = email
         this.dataNasc = dataNasc
         this.senha = senha
+        this.avatar = avatar
     }
 }
